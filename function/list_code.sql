8458e77a72a841bb0850d767f33e79ba2e0b2a2b
THIRUMAL <m.thirumal@hotmail.com>
2017-02-22 12:05:13 +0530

    Added raise notice

--- function/list_code.sql
+++ function/list_code.sql
@@ -1,9 +1,15 @@
 CREATE OR REPLACE FUNCTION list_code(input_table_name varchar, locale integer) RETURNS table(table_names varchar) AS $$
 DECLARE 
 	found_table varchar;
+	row_count bigint;
 BEGIN 
-	return query SELECT table_name::varchar FROM information_schema.tables WHERE table_schema = 'code' AND table_name like input_table_name;
+	SELECT INTO row_count count(*) FROM information_schema.tables WHERE table_schema = 'code' AND table_name like input_table_name;
+	IF row_count > 0 THEN 
+		return query SELECT table_name::varchar FROM information_schema.tables WHERE table_schema = 'code' AND table_name like input_table_name;
+	ELSE 
+		RAISE NOTICE 'Table not found';
+	END IF;
 END; 
- $$ 
+$$ 
 LANGUAGE 'plpgsql';
  
