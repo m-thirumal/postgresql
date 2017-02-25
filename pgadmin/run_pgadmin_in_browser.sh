0445b2fdf8c6097d76ea4ba2590f15d3b2ff8991
thirumal <thirumal@Thirumal>
2017-02-25 13:39:07 +0530

    Added nohup

--- pgadmin/run_pgadmin_in_browser.sh
+++ pgadmin/run_pgadmin_in_browser.sh
@@ -2,7 +2,7 @@ cd
 virtualenv pgadmin4
 cd pgadmin4
 source bin/activate
-python lib/python2.7/site-packages/pgadmin4/pgAdmin4.py
+nohup python lib/python2.7/site-packages/pgadmin4/pgAdmin4.py &
 
 
 
