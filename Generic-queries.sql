dd524a5183a8c018ad21d222c53842aa7a92f3e3
THIRUMAL <m.thirumal@hotmail.com>
2017-06-28 15:42:39 +0530

    Added track / transaction queries

--- Generic-queries.sql
+++ Generic-queries.sql
@@ -1,2 +1,18 @@
 ---To get all the details about postgresql (here to get total number of rows in all tables)
 SELECT schemaname, relname, n_live_tup FROM pg_stat_user_tables where schemaname = 'code' ORDER BY n_live_tup DESC; 
+
+-- Show database size_
+SELECT pg_database.datname, pg_size_pretty(pg_database_size(pg_database.datname)) AS size FROM pg_database;
+SELECT pg_size_pretty(pg_database_size('indsolv'));
+
+---Show size of table size_
+SELECT pg_size_pretty(pg_relation_size('code.address_type_cd'));
+
+---Reload configuration 
+select pg_reload_conf();
+
+-- To get last commit/transaction Date of table
+select pg_xact_commit_timestamp(xmin), * from indsolv.address
+
+-- To get last commit/transaction Date of column
+SELECT pg_xact_commit_timestamp(xmin), * FROM YOUR_TABLE_NAME where COLUMN_NAME=VALUE;
--- admin/useful_queries.md
+++
@@ -1,9 +0,0 @@
-_Show database size_
-> SELECT pg_database.datname, pg_size_pretty(pg_database_size(pg_database.datname)) AS size FROM pg_database;
-> SELECT pg_size_pretty(pg_database_size('ICMS'));
-
-_Show size of table size_
-> SELECT pg_size_pretty(pg_relation_size('code.address_type_cd'));
-
-Reload configuration 
-> select pg_reload_conf();
