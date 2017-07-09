376ad168a94ef343f4e062f7179e24edc5aa3834
THIRUMAL <m.thirumal@hotmail.com>
2017-07-09 10:49:54 +0530

    Modified

--- Generic-queries.sql
+++ Generic-queries.sql
@@ -27,3 +27,8 @@ SELECT current_date;
 SET datestyle TO postgres, dmy;
 SET datestyle TO postgres, mdy;
 SET datestyle TO ISO, dmy;
+---Get all scehma name
+select schema_name from information_schema.schemata
+select nspname from pg_catalog.pg_namespace;
+---list of schema with sizes 
+SELECT schema_name, pg_size_pretty(sum(table_size)::bigint) as "disk space", (sum(table_size) / pg_database_size(current_database())) * 100 as "percent" FROM (SELECT pg_catalog.pg_namespace.nspname as schema_name, pg_relation_size(pg_catalog.pg_class.oid) as table_size FROM   pg_catalog.pg_class JOIN pg_catalog.pg_namespace ON relnamespace = pg_catalog.pg_namespace.oid) t GROUP BY schema_name ORDER BY schema_name
