1d024637dc8c0464a2711467b5e915687618cf45
THIRUMAL <m.thirumal@hotmail.com>
2017-10-15 21:28:46 +0530

    u

--- Generic-queries.sql
+++ Generic-queries.sql
@@ -32,3 +32,8 @@ select schema_name from information_schema.schemata
 select nspname from pg_catalog.pg_namespace;
 ---list of schema with sizes 
 SELECT schema_name, pg_size_pretty(sum(table_size)::bigint) as "disk space", (sum(table_size) / pg_database_size(current_database())) * 100 as "percent" FROM (SELECT pg_catalog.pg_namespace.nspname as schema_name, pg_relation_size(pg_catalog.pg_class.oid) as table_size FROM   pg_catalog.pg_class JOIN pg_catalog.pg_namespace ON relnamespace = pg_catalog.pg_namespace.oid) t GROUP BY schema_name ORDER BY schema_name
+---DELETE All the tables from schema
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+GRANT ALL ON SCHEMA public TO administrator;
+GRANT ALL ON SCHEMA public TO public;
